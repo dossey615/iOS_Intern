2ee2975bb3a635ca0aede1c17e7346c1f6702247
dossey615 <dossey615@gmail.com>
2018-05-01 14:45:19 +0900

    Initial Commit

---
+++ iOS_InternTests/iOS_InternTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_InternTests.swift
+//  iOS_InternTests
+//
+//  Created by 土居将史 on 2018/05/01.
+//  Copyright © 2018年 土居将史. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Intern
+
+class iOS_InternTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
